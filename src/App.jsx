b0c57b40c904bfe602c0a7f235a006e778db4559
vahid haidary <vahidhaidary0077@gmail.com>
2025-03-04 21:26:51 +0330

    font-config

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ function App() {
 
   return (
     <>
-      <div className=" bg-primary w-4 h-9 " >MyProject</div>
+      <div className=" bg-primary w-full h-9 font-dana-bold " >سلام من وحید هستم</div>
+      <div className=" bg-primary w-full h-9 font-dana-base " >سلام من وحید هستم</div>
+      <div className=" bg-primary w-full h-9 font-dana-reg " >سلام من وحید هستم</div>
+
+      <div className=" bg-primary w-full h-9 font-kalameh-reg " >سلام من وحید هستم</div>
+      <div className=" bg-primary w-full h-9 font-kalameh-bold " >سلام من وحید هستم</div>
+      <div className=" bg-primary w-full h-9 font-kalameh-medium " >سلام من وحید هستم</div>
+      <div className=" bg-primary w-full h-9 font-kalameh-extra " >سلام من وحید هستم</div>
+
+
     </>
   )
 }
